b2ee51d50664cc20f2cd0c4816532fac6c4c0994
Dmitry Volkov <83473625+Ocsidsus@users.noreply.github.com>
2022-02-13 13:27:06 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,5 +1,17 @@
 // Copyright 2021 NNTU-CS
 
 int gcd(int a, int b) {
-    // поместить сюда текст реализации алгоритма (на С++)
+    int min = 0;
+	if (a > b) {
+		min = b;
+	}
+	else {
+		min = a;
+	}
+
+	for (int i = min; i > 0; i--) {
+		if ((a % i == 0) && (b % i == 0)) {
+			return i;
+		}
+	}
 }
